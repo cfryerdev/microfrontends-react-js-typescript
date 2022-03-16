9c4492fc7d4831ec20842b7512f9e18efc3e3d27
Chris Fryer <cfryerdev@gmail.com>
2022-03-16 12:06:16 -0600

    fixed profile not having suspense due to layout omit

--- host/src/pages/profile/index.tsx
+++ host/src/pages/profile/index.tsx
@@ -1,9 +1,13 @@
 
-import React from "react";
+import React, { Suspense } from "react";
 
 // @ts-ignore
 const ProfileRemote = React.lazy(() => import("remote_profile/Application"));
 
-const ProfilePage = () => (<ProfileRemote />);
+const ProfilePage = () => (
+    <Suspense fallback={<div>Loading....</div>}>
+        <ProfileRemote />
+    </Suspense>
+);
 
 export default ProfilePage;
