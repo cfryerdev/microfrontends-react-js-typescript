b7bdd7cd663290676ca2eaf9500f205c4443337e
hb-coding <hberge17@gmail.com>
2022-09-01 08:22:49 -0600

    adding initial example

--- host/src/_app.tsx
+++ host/src/_app.tsx
@@ -1,18 +1,42 @@
 import { BrowserRouter } from "react-router-dom";
 import TelemetryListener from "./components/telemetry/TelemetryListener";
 import Routing from "./_routing";
-import { ErrorBoundary } from "react-error-boundary";
-import { createContext } from "react";
+import { AuthProvider, useAuth } from "react-oidc-context";
 
 
+function Login() {
+  const auth = useAuth();
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        Hello {auth.user?.profile.sub}{" "}
+        <button onClick={() => void auth.removeUser()}>
+          Log out
+        </button>
+      </div>
+    );
+  }
+
+  return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+}
 const HostRouter = () => (
 
-  <>
+  <AuthProvider {...oidcConfig}>
+    <Login />
     <TelemetryListener />
     <BrowserRouter>
       <Routing />
     </BrowserRouter>
-  </>
+  </AuthProvider>
 );
 
 export default HostRouter;
